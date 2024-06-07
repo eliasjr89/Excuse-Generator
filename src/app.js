e1740ed1fb403db21dc83788ed4c6f31aebc752f
Elías JR <167987056+eliasjr1989@users.noreply.github.com>
2024-06-07 13:58:59 +0000

    ExcuseGenerator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function excuseGenerator() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  function randomExcuse(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+  let excuse = `${randomExcuse(who)} ${randomExcuse(action)} ${randomExcuse(
+    what
+  )} ${randomExcuse(when)}`;
+  return excuse;
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.getElementById("excuse").innerHTML = excuseGenerator();
